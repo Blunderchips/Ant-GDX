2d476e7c619fee96283bca666269e57ae75652df
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-01-13 12:31:06 +0200

    Generated libGDX project

---
+++ ant-gdx/desktop/src/dot/empire/ants/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dot.empire.ants.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dot.empire.ants.Ants;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ants(), config);
+	}
+}
